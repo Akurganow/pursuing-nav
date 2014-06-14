f3aee3c33bae7dd8589f45226357390a99c03cfe
Akurganow <akurganow@polzo.ru>
2014-06-14 05:14:36 +0400

    Change plugin to version 0.2

--- js/jquery.pursuingsnav.js
+++ js/jquery.pursuingsnav.js
@@ -1,59 +1,80 @@
+/*!
+* Pursuing Nav v0.2 (http://akurganow.github.io/pursuing-nav)
+* Copyright 2013-2014 Alexander Kurganov.
+* Licensed under GPL License (http://www.gnu.org/licenses/gpl.html)
+*/
+
 (function($) {
 	$.fn.pursuingsnav = function(){
-		var element = this, //приклеивающийся элемент
-				height = element.outerHeight(), //высота элемента
-				width = element.outerWidth(), //ширина элемента
-				offsetTop = element.offset().top, //отступ от верхней границы документа
-				offsetLeft = element.offset().left, //отступ от левой границы элемента
-				stick = height+offsetTop; //момент приклеивания
-				presc = 0, //зададим переменную для вычисления направления движения
-				delta = 0; //зададим переменную дельты
-		log('*Element height*: '+height+'\n*Element width*: '+width+'\n*Element offset top*: '+offsetTop+'\n*Element offset left*: '+offsetLeft+'\n*Element stick moment*: '+stick);
-		if(!element.hasClass('navbar-fixed-top')){
-			element.addClass('navbar-fixed-top');
-		}
-		element.css({ //задаем css
-			position: 'absolute',
-			top: offsetTop
-		});
+		var userAgent = navigator.userAgent,
+				is_pursuing = true,
+				is_fixed = false;
 
-		$(window).scroll(function(){
-			var sc = $(document).scrollTop(),
-					height = element.outerHeight(), //высота элемента
-					width = element.outerWidth(), //ширина элемента
-					offsetTop = element.offset().top, //отступ от верхней границы документа
-					offsetLeft = element.offset().left, //отступ от левой границы элемента
-					stick = height+offsetTop; //момент приклеивания
-			if(sc>0){
-				if(presc<sc){delta = -1}
-				else if(presc>sc){delta = 1}
-  			else{delta = 0};
-			}
-			if(delta<0){// скрол вниз
-				if(sc == element.offset().top){
-					element.css({
-						position: 'absolute',
-						top:sc
-					});
+		if(/android|iphone|ipad/i.test(userAgent)){
+			is_pursuing = false,
+			is_fixed = true;
+		}
+
+		var element = this,
+				height = element.outerHeight(),
+				width = element.outerWidth(),
+				offsetTop = element.offset().top,
+				offsetLeft = element.offset().left,
+				stick = height+offsetTop;
+				presc = 0,
+				delta = 0;
+
+		if(is_pursuing){
+			element.css({
+				position: 'absolute',
+				top: offsetTop
+			});
+
+			$(window).scroll(function(){
+				var sc = $(document).scrollTop()
+						height = element.outerHeight(),
+						width = element.outerWidth(),
+						offsetTop = element.offset().top,
+						offsetLeft = element.offset().left,
+						stick = height+offsetTop;
+				if(sc>0){
+					if(presc<sc){delta = -1}
+					else if(presc>sc){delta = 1}
+					else{delta = 0};
 				}
-				else if(sc>stick){
-					element.css({
-						position: 'absolute',
-						top: sc-height
-					});
+				if(delta<0){
+					if(sc == element.offset().top){
+						element.css({
+							position: 'absolute',
+							top:sc
+						});
+					}
+					else if(sc>stick){
+						element.css({
+							position: 'absolute',
+							top: sc-height
+						});
+					}
+
 				}
-				
-			}
-			else if(delta>0){//скрол вверх
-				stick = height+element.offset().top;
-				if(sc <= element.offset().top){
-					element.css({
-						position: 'fixed',
-						top: 0
-					});
+				else if(delta>0){
+					stick = height+element.offset().top;
+					if(sc <= element.offset().top){
+						element.css({
+							position: 'fixed',
+							top: 0
+						});
+					}
 				}
-			}
-			presc = sc;
-		});
+				presc = sc;
+			});
+		}
+		else if(is_fixed){
+			var element = this;
+			element.css({
+				position: 'fixed',
+				top: offsetTop
+			});
+		}
 	};
 })(jQuery);
