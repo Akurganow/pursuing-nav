47b161d6d259db55f36b88cb3111abce151566ac
Akurganow <akurganow@polzo.ru>
2014-06-14 04:40:51 +0400

    Many fixes
    Add license
    Update readme
    Pursuing nav on iPhone,iPad & Android just Fixed nav

--- jquery.pursuingsnav.js
+++ jquery.pursuingsnav.js
@@ -1,63 +1,80 @@
-//TODO: Добавить опций, не преследующее, а просто анимированное меню
-//TODO: Добавить возможность отслеживать событие скрола (вверх/вниз)
-//TODO: Перевести на английский
+/*!
+ * Pursuing Nav v0.2 (http://akurganow.github.io/pursuing-nav)
+ * Copyright 2013-2014 Alexander Kurganov.
+ * Licensed under Creative Commons «Attribution-ShareAlike» 4.0 (http://creativecommons.org/licenses/by-sa/4.0/)
+ */
+
 (function($) {
-	$.fn.pursuingsnav = function(){
-		var element = this, //приклеивающийся элемент
-				height = element.outerHeight(), //высота элемента
-				width = element.outerWidth(), //ширина элемента
-				offsetTop = element.offset().top, //отступ от верхней границы документа
-				offsetLeft = element.offset().left, //отступ от левой границы элемента
-				stick = height+offsetTop; //момент приклеивания
-				presc = 0, //зададим переменную для вычисления направления движения
-				delta = 0; //зададим переменную дельты
-		if(!element.hasClass('navbar-fixed-top')){
-			element.addClass('navbar-fixed-top'); //Если у элемента нет класса .navbar-fixed-top добавим
-		}
-		element.css({ //задаем css
-			position: 'absolute',
-			top: offsetTop
-		});
+  $.fn.pursuingsnav = function(){
+    var userAgent = navigator.userAgent,
+        is_pursuing = true,
+        is_fixed = false;
 
+    if(/android|iphone|ipad/i.test(userAgent)){
+      is_pursuing = false,
+      is_fixed = true;
+    }
 
+		var element = this,
+				height = element.outerHeight(),
+				width = element.outerWidth(),
+				offsetTop = element.offset().top,
+				offsetLeft = element.offset().left,
+				stick = height+offsetTop;
+				presc = 0,
+				delta = 0;
 
-		$(window).scroll(function(){
-			var sc = $(document).scrollTop()
-					height = element.outerHeight(), //высота элемента
-					width = element.outerWidth(), //ширина элемента
-					offsetTop = element.offset().top, //отступ от верхней границы документа
-					offsetLeft = element.offset().left, //отступ от левой границы элемента
-					stick = height+offsetTop; //момент приклеивания;
-			if(sc>0){
-				if(presc<sc){delta = -1}
-				else if(presc>sc){delta = 1}
-  			else{delta = 0};
-			}
-			if(delta<0){// скрол вниз
-				if(sc == element.offset().top){
-					element.css({
-						position: 'absolute',
-						top:sc
-					});
-				}
-				else if(sc>stick){
-					element.css({
-						position: 'absolute',
-						top: sc-height
-					});
-				}
-				
-			}
-			else if(delta>0){//скрол вверх
-				stick = height+element.offset().top;
-				if(sc <= element.offset().top){
-					element.css({
-						position: 'fixed',
-						top: 0
-					});
-				}
-			}
-			presc = sc;
-		});
-	};
+    if(is_pursuing){
+      element.css({ //задаем css
+        position: 'absolute',
+        top: offsetTop
+      });
+
+      $(window).scroll(function(){
+        var sc = $(document).scrollTop()
+            height = element.outerHeight(), //высота элемента
+            width = element.outerWidth(), //ширина элемента
+            offsetTop = element.offset().top, //отступ от верхней границы документа
+            offsetLeft = element.offset().left, //отступ от левой границы элемента
+            stick = height+offsetTop; //момент приклеивания;
+        if(sc>0){
+          if(presc<sc){delta = -1}
+          else if(presc>sc){delta = 1}
+          else{delta = 0};
+        }
+        if(delta<0){// скрол вниз
+          if(sc == element.offset().top){
+            element.css({
+              position: 'absolute',
+              top:sc
+            });
+          }
+          else if(sc>stick){
+            element.css({
+              position: 'absolute',
+              top: sc-height
+            });
+          }
+
+        }
+        else if(delta>0){//скрол вверх
+          stick = height+element.offset().top;
+          if(sc <= element.offset().top){
+            element.css({
+              position: 'fixed',
+              top: 0
+            });
+          }
+        }
+        presc = sc;
+      });
+    }
+    else if(is_fixed){
+      var element = this;
+      element.css({ //задаем css
+        position: 'fixed',
+        top: offsetTop
+      });
+    }
+  };
 })(jQuery);
--- readme.md
+++ readme.md
@@ -2,10 +2,17 @@ Pursuings nav
 ===
 
 ###Usage
+```html
+<script src="//"></script>
+```
+
 ```js
-$('#main-nav').pursuingsnav();
+$(function(){
+  $('#main-nav').pursuingsnav();
+});
 ```
 
 * [Demo](http://akurganow.github.io/pursuing-nav)
 
 ![TODOs by CodeNotes](http://codenotes.me/badge/12768920/todos.png)
+[![LICENSE](http://i.creativecommons.org/l/by-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-sa/4.0/)
